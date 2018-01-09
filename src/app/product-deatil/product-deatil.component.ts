ed2cb9f92251f0f4e9f9bd36d5e5b398bdb786b5
Deft-pawN <799397253@qq.com>
2018-01-09 05:53:29 +0000

    20170109

--- src/app/product-deatil/product-deatil.component.ts
+++ src/app/product-deatil/product-deatil.component.ts
@@ -8,12 +8,12 @@ import { ProductService,Product } from '../share/product.service';
   styleUrls: ['./product-deatil.component.css']
 })
 export class ProductDeatilComponent implements OnInit {
-  //productTitle :string; 
+  //productTitle :string; receive product 
   product:Product;
   constructor(private routerInfo:ActivatedRoute,private productService:ProductService) { }
 
   ngOnInit() {
-    //参数快照 
+    //参数快照,获得新的 
     let productId:number = this.routerInfo.snapshot.params["ProductId"];
     this.product = this.productService.getProduct(productId)//get the id of product
     
